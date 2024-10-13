834d9285c3c65cece7af89b01e3087a0733c2f8c
Nar18PP <vinakasi555@gmail.com>
2024-10-13 11:24:54 +0700

    Update server.js with new changes

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.get('/api/products', (req, res) => {
       { id: 5, name: 'ສະມູດຕີ່', heart: 1672, price: 54, img:"https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=600" },
       { id: 6, name: 'ເຄັກຊອກໂກແລັດ', heart: 541, price: 210, img:"https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=600" },
       { id: 7, name: 'ຊີ້ນໝາ', heart: 662, price: 56, img:"https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&w=600" },
-      { id: 11, name: 'ຊູຊິ', heart: 25563, price: 156, img:"https://images.pexels.com/photos/357756/pexels-photo-357756.jpeg?auto=compress&cs=tinysrgb&w=600" },
-      { id: 8, name: 'ຊີ້ນງົວ', heart: 954, price: 84, img:"https://images.pexels.com/photos/793785/pexels-photo-793785.jpeg?auto=compress&cs=tinysrgb&w=600https://images.pexels.com/photos/769290/pexels-photo-769290.jpeg?auto=compress&cs=tinysrgb&w=600" },
-      { id: 9, name: 'ໄຂ່ຕົ້ມ', heart: 2359, price: 59, img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZ8Hv38DtbZs2gqhTLkKT-MgbmHTHpdHVw&s" },
-      { id: 10, name: 'ພິດຊ່າ', heart: 587, price: 85, img:"https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?cs=srgb&dl=pexels-vince-2147491.jpg&fm=jpg" },
+      { id: 8, name: 'ຊູຊິ', heart: 25563, price: 156, img:"https://images.pexels.com/photos/357756/pexels-photo-357756.jpeg?auto=compress&cs=tinysrgb&w=600" },
+      { id: 9, name: 'ຊີ້ນງົວ', heart: 954, price: 84, img:"https://images.pexels.com/photos/793785/pexels-photo-793785.jpeg?auto=compress&cs=tinysrgb&w=600https://images.pexels.com/photos/769290/pexels-photo-769290.jpeg?auto=compress&cs=tinysrgb&w=600" },
+      { id: 10, name: 'ໄຂ່ຕົ້ມ', heart: 2359, price: 59, img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZ8Hv38DtbZs2gqhTLkKT-MgbmHTHpdHVw&s" },
+      { id: 11, name: 'ພິດຊ່າ', heart: 587, price: 85, img:"https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?cs=srgb&dl=pexels-vince-2147491.jpg&fm=jpg" },
     ];
+    for (let i = 12; i <= 20; i++) {
+      products.push({ id: i, name: `Product ${i}`, heart: Math.floor(Math.random() * 1000), price: Math.floor(Math.random() * 100) + 1, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZ8Hv38DtbZs2gqhTLkKT-MgbmHTHpdHVw&s" });
+    }
     res.json(products);
   });
 
