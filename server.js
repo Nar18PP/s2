041de5f8e79f9154119ce97def74cd73ed191e44
Nar18PP <vinakasi555@gmail.com>
2024-10-25 00:59:17 +0700

    'kkk'

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/set-cookie", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // อนุญาตเฉพาะต้นทางนี้
+    origin: "*", // อนุญาตเฉพาะต้นทางนี้
     methods: ["GET", "POST"],
     credentials: true,
   },
